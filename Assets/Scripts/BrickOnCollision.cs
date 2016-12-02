ad3fbdbde97daaefad5d071ad0e88094d217c6a0
justbert <justin.m.bertrand@gmail.com>
2016-12-02 18:32:19 -0500

    Initial Commit

---
+++ Assets/Scripts/BrickOnCollision.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickOnCollision : MonoBehaviour
+{
+
+    public float explosiveForce;
+    public float explosionRadius;
+    private MoveSphere move;
+    public GameObject particle;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    explosiveForce = 500.0f;
+	    explosionRadius = 2.0f;
+        GameObject ball = GameObject.Find("Ball");
+        move = ball.GetComponent<MoveSphere>();
+	    particle = GameObject.Find("SparkEmitter");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Ball")
+        {
+            Rigidbody body = GetComponent<Rigidbody>();
+
+            body.AddExplosionForce(explosiveForce*move.speed, collision.transform.position, explosionRadius*move.speed);
+
+            GameObject newParticle = Instantiate(particle, transform.position, Quaternion.identity) as GameObject;
+
+            newParticle.GetComponent<ParticleSystem>().Play();
+        }
+
+    }
+}
---
+++ Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MoveSphere : MonoBehaviour
+{
+
+    public float acceleration;
+    public float speed;
+    public float angleSpeed;
+    public float angle = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    acceleration = 10.0f;
+	    speed = 0.0f;
+	    angleSpeed = 100.0f;
+	}
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed += acceleration*Time.deltaTime;
+        }
+        else if(speed > 0)
+        {
+            speed -= acceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            speed -= acceleration*Time.deltaTime;
+        }
+        else if(speed < 0)
+        {
+            speed += acceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            angle -= Time.deltaTime * angleSpeed;
+            transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            angle += Time.deltaTime * angleSpeed;
+            transform.localRotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        }
+
+        transform.localPosition += (transform.forward * speed * Time.deltaTime);
+    }
+
+    
+}
